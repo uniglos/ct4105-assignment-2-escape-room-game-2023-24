4af963e26b3b7d3c4d5ea36e59e70bd91843460a
Zayd Dawood <zdawood@glos.ac.uk>
2024-03-07 09:43:20 +0000

    Added Puzzle Scene Management

---
+++ Assets/Scripts/PuzzleManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PuzzleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<string> puzzleNames;
+
+    public TextMeshProUGUI combinationLock;
+
+    private string lockCode;
+
+    void Start()
+    {
+
+        if( combinationLock )
+        {
+
+            lockCode = combinationLock.text;
+
+            combinationLock.text = "";
+
+        }
+
+        createPuzzleList();
+
+    }
+    
+    public void createPuzzleList()
+    {
+
+        foreach( string puzzle in puzzleNames)
+        {
+
+            if( !PlayerPrefs.HasKey( puzzle ) )
+            {
+
+                PlayerPrefs.SetString( puzzle, "" );
+
+            }
+            
+        }
+
+        checkPuzzleStatus();
+
+    }
+
+    public void checkPuzzleStatus()
+    {
+
+        int numberOfPuzzlesComplete = 0;
+
+        foreach( string puzzle in puzzleNames)
+        {
+
+            var status = "Incomplete";
+
+            if( PlayerPrefs.GetString( puzzle ) != "" )
+            {
+
+                status = "Complete";
+
+                numberOfPuzzlesComplete++;
+                
+            }
+            
+            print( "Puzzle " + puzzle + " is: " + status);
+
+        }
+    
+        if( combinationLock )
+        {
+            
+            combinationLock.text = lockCode.Substring( 0, numberOfPuzzlesComplete );
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void deletePlayerPrefs()
+    {
+
+        PlayerPrefs.DeleteAll();
+
+        print( "PlayerPrefs deleted." );
+
+    }
+
+}
---
+++ Assets/Scripts/Puzzles/PuzzleOne.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PuzzleOne : MonoBehaviour
+{
+
+    public string nameOfPuzzle, escapeRoomGameScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void goBackToEscapeRoom( string escapeRoomGameScene )
+    {
+       
+        PlayerPrefs.SetInt( "Score", PlayerPrefs.GetInt( "Score" ) + 1 );
+
+        PlayerPrefs.SetString( nameOfPuzzle, "Complete" );
+
+        PlayerPrefs.Save();
+
+        SceneManager.LoadScene( escapeRoomGameScene );   
+
+    }
+
+}
